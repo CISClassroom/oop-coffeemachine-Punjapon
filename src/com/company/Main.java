d7c25a44d131b42e154f067acbfcf0ee1114268d
นาย ปัญจพล อ่อนโคทา <58883256+Punjapon@users.noreply.github.com>
2020-03-08 02:33:22 +0700

    100%

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	 CoffeeMachine coffeeMachine = new CoffeeMachine();
-	 coffeeMachine.Makecoffee(Americano:6);
+    public static  void  main(String [] arge ){
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.MakeCoffee(6);
     }
 }
